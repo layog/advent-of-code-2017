e917c7da5a1836c2c727911751bc540eb5c81b43
Ankit Goyal <goyalankit40@gmail.com>
2017-12-20 22:44:44 +0530

    Add complete solutions to day 5

---
+++ q5_2.py
@@ -0,0 +1,16 @@
+with open("in.txt", "r") as in_file:
+    jump_instructions = map(int, (in_file.read()).strip().split())
+
+total_jumps = 0
+current_index = 0
+
+while 0 <= current_index < len(jump_instructions):
+    total_jumps += 1
+    past_index = current_index
+    current_index += jump_instructions[past_index]
+    if jump_instructions[past_index] >= 3:
+        jump_instructions[past_index] -= 1
+    else:
+        jump_instructions[past_index] += 1
+
+print total_jumps
