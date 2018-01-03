85a53fa818c29e90bb64216b7d9347b4555be9ef
Ankit Goyal <goyalankit40@gmail.com>
2018-01-03 20:00:36 +0530

    Clean solutions to day 1

--- q1.py
+++ q1.py
@@ -1,7 +1,8 @@
+# Reading the number
 inFile = open("in.txt", "r")
-
 number = inFile.readline().strip()
 inFile.close()
+
 ans = 0
 for index in range(len(number)):
     if number[index] == number[(index+1) % (len(number))]:
--- q1_2.py
+++ q1_2.py
@@ -1,9 +1,11 @@
+# Reading the number
 inFile = open("in.txt", "r")
-
 number = inFile.readline().strip()
 inFile.close()
+
 ans = 0
 for index in range(len(number)):
+    # OK simply add half the number of the digits
     if number[index] == number[(index+ len(number)/2) % (len(number))]:
         ans += int(number[index])
 
